aa521984d3883e398e9d0a01efb8b54acb33b96a
RandyWei <weizhengheng@chinahrt.com>
2023-08-22 17:09:45 +0800

    增加注释及readme

--- Wechat/src/commonMain/kotlin/icu/bughub/kit/multiplatform/wechat/Wechat.kt
+++ Wechat/src/commonMain/kotlin/icu/bughub/kit/multiplatform/wechat/Wechat.kt
@@ -4,8 +4,8 @@ expect object Wechat {
     /**
      * 分享
      *
-     * @param mediaMessage
-     * @param scene
+     * @param mediaMessage [MediaMessage]和官方属性是一一对应的WXMediaMessage，其中mediaObject也是和官方一一对应的
+     * @param scene [WXScene]分享场景
      */
     fun share(mediaMessage: MediaMessage, scene: WXScene)
 }
