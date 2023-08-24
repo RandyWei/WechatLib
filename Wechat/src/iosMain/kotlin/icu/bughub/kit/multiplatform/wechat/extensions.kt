b50464bd08def157893110e06f22a8f936f11f08
RandyWei <weizhengheng@chinahrt.com>
2023-08-24 11:07:47 +0800

    升级kotlin->1.9.0

--- Wechat/src/iosMain/kotlin/icu/bughub/kit/multiplatform/wechat/extensions.kt
+++ Wechat/src/iosMain/kotlin/icu/bughub/kit/multiplatform/wechat/extensions.kt
@@ -1,10 +1,13 @@
 package icu.bughub.kit.multiplatform.wechat
 
+import kotlinx.cinterop.BetaInteropApi
+import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.allocArrayOf
 import kotlinx.cinterop.memScoped
 import platform.Foundation.NSData
 import platform.Foundation.create
 
+@OptIn(ExperimentalForeignApi::class, BetaInteropApi::class)
 fun ByteArray.toNSData(): NSData = memScoped {
     NSData.create(bytes = allocArrayOf(this@toNSData), length = this@toNSData.size.toULong())
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.library").version("8.1.0").apply(false)
-    kotlin("multiplatform").version("1.8.21").apply(false)
+    kotlin("multiplatform").version("1.9.0").apply(false)
 }
 
 tasks.register("clean", Delete::class) {
